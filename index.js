8bf531f9897032681f1e5c03facc9a7219e17d0d
20261664 <20261664@ctucareer.co.za>
2026-03-04 11:40:07 +0200

    Update server listen address and correct repository URLs in package.json

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(cookieParser());
 
 app.use('/', ctuRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
