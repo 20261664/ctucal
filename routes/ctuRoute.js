2011d95494d23159a82580386513d3982d31ceb2
20261664 <20261664@ctucareer.co.za>
2026-03-13 11:04:57 +0200

    fix: add family option to nodemailer transport configuration

--- routes/ctuRoute.js
+++ routes/ctuRoute.js
@@ -195,6 +195,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.SENDER_EMAIL,
     pass: process.env.SENDER_PASSWORD
